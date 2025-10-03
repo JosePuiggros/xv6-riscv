69d56e93d1bd6efc9e74765975915f113f8e8029
JosePuiggros <josepuiggrost@gmail.com>
2025-10-03 17:02:18 -0300

    Tarea 1

---
+++ xv6-riscv/user/yosoytupadre.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "user.h"
+
+int main() {
+  int pid = fork();
+  if (pid < 0) {
+    printf("Fork failed\n");
+    exit(1);
+  } else if (pid == 0) {
+    // Proceso hijo
+    printf("Hola, soy el proceso: PID = %d, mi padre tiene el: ID = %d\n", getpid(), getppid());
+    exit(0);
+  } else {
+    // Proceso padre
+    wait(0); // Esperar a que el hijo termine
+    printf("Hola, soy el proceso: PID = %d, mi hijo tiene el: ID = %d\n", getpid(), pid);
+  }
+  exit(0);
+}
